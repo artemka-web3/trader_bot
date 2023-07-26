54ef095bfc24ac3ce253ddc8b3c1286403123c33
Артем <artemsidnev@MacBook-Pro-Artem.local>
2023-07-26 10:58:42 +0300

    fix

--- main.py
+++ main.py
@@ -405,6 +405,10 @@ async def process_stock(stock, volume_avg_prev, coef):
                     pass
                 else:
                     await asyncio.sleep(25)
+                    current_date = (datetime.now(offset)).strftime('%Y-%m-%d')
+                    current_hour = ("0" +str(datetime.now(offset).hour) if len(str(datetime.now(offset).hour)) < 2 else str(datetime.now(offset).hour))
+                    current_minute = ("0" +str(datetime.now(offset).minute) if len(str(datetime.now(offset).minute)) < 2 else str(datetime.now(offset).minute))
+                    current_second = ("0" +str(datetime.now(offset).second) if len(str(datetime.now(offset).second)) < 2 else str(datetime.now(offset).second))
                 users_arr = db.get_all_users()
                 current_time = str(current_hour) +":"+ str(current_minute)
                 stock_data = await moex_async.get_stock_data(stock[0]) 
