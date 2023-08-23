a1a27abcef4d7a17ccd760526aef15c84095f9ba
artemka-web3 <artemkasidnev.web3@gmail.com>
2023-08-24 02:35:10 +0300

    server opt adding

---
+++ server_opt.py
@@ -0,0 +1,20 @@
+import os
+import time
+import schedule
+
+def job():
+    os.system('sudo systemctl stop collect_bit.service')
+    time.sleep(2)
+    os.system('sudo apt clean')
+    time.sleep(2)
+    os.system('sudo apt autoremove')
+    time.sleep(5)
+    os.system('sudo rm -rf /tmp/*')
+    time.sleep(5)
+    time.sleep('sudo systemctl start collect_bit.service')
+
+schedule.every(10).minutes.do(job)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
