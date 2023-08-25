0d7e8573e7b6bd99c99acc234bb146771b7c8b32
artemka-web3 <artemkasidnev.web3@gmail.com>
2023-08-26 00:12:20 +0300

    test

--- collect_big_volumes.py
+++ collect_big_volumes.py
@@ -28,7 +28,7 @@ def collect_stocks():
         thread = threading.Thread(target=share_thread, args=(task[0], task[1],))
         threads.append(thread)
         thread.start()
-        time.sleep(0.3)
+        #time.sleep(0.05)
 
 def share_thread(stock, coef):
     while True:
@@ -66,7 +66,7 @@ def share_thread(stock, coef):
                     dir = "🔴"
                 check_volume = volume_avg_prev[stock[0]]
                 logging.info(f"{stock[0]} volume: {volume_rub} coef: {coef} check_volume: {check_volume} result: {bool(coef*check_volume<volume_rub)}")
-                if check_volume * coef < volume_rub and volume_rub > 1000000:
+                if check_volume * 0 < volume_rub and volume_rub > 1000000:
                     print('Повышенные ', stock[0])
                     data = {
                         "sec_id": sec_id,
--- server_opt.py
+++ server_opt.py
@@ -3,17 +3,15 @@ import time
 import schedule
 
 def job():
-    os.system('sudo systemctl stop collect_big.service')
-    time.sleep(2)
     os.system('sudo apt clean')
     time.sleep(2)
     os.system('sudo apt autoremove')
     time.sleep(2)
     os.system('sudo rm -rf /tmp/*')
     time.sleep(2)
-    os.system('sudo systemctl start collect_big.service')
+    os.system('sudo systemctl restart collect_big.service')
 
-schedule.every(10).minutes.at(':00').do(job)
+schedule.every(3).minutes.at(':00').do(job)
 
 while True:
     schedule.run_pending()
