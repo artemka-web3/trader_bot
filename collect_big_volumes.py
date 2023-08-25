660cab44a6ee9fcd6807756afb3ec67b6a3f8f4a
artemka-web3 <artemkasidnev.web3@gmail.com>
2023-08-25 18:56:21 +0300

    fix

--- collect_big_volumes.py
+++ collect_big_volumes.py
@@ -27,7 +27,7 @@ def collect_stocks():
         thread = threading.Thread(target=share_thread, args=(task[0], task[1],))
         threads.append(thread)
         thread.start()
-        time.sleep(0.15)
+        time.sleep(0.3)
 
 def share_thread(stock, coef):
     while True:
